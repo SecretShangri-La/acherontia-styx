5c429a46af45dddf0b9d3b924e80bb3415366f45
DR_KREUZ <48596072+SecretShangri-La@users.noreply.github.com>
2023-01-17 11:45:56 +0900

    Update RUNME.sh

--- RUNME.sh
+++ RUNME.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo -e "\e[32m [+] Download Dockerfile"
 git clone https://github.com/SecretShangri-La/acherontia-styx weaponX
+if [ "$(pwd)/weaponX/$0" ]; then
+  rm $(pwd)/weaponX/$0
+fi
